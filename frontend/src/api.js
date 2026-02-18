629c6536cc1cf56a856e6f131c68c7b8042757e7
PARTHA SINGHA <145651386+Parthaoff@users.noreply.github.com>
2026-02-18 23:34:02 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://my-blog-app-plpl.onrender.com',
+  baseURL: 'https://my-blog-app-1-fszm.onrender.com',
 });
 
 // This automatically adds the token to requests if we have one saved
@@ -14,3 +14,4 @@ API.interceptors.request.use((req) => {
 
 
 export default API;
+
